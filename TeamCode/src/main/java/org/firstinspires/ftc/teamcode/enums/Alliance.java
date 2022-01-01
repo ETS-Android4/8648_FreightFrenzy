fac45d7ef52e5bb7ef5aa87ffa00ceee7c6f2af4
kirstenpolk10 <kirstenpolk10@gmail.com>
2022-01-01 13:45:47 -0600

    Added all of the enums from 16244 repo! (thanks for those)

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/enums/Alliance.java
@@ -0,0 +1,7 @@
+package org.firstinspires.ftc.teamcode.enums;
+
+public enum Alliance {
+    RED,
+    BLUE
+}
+
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/enums/GameElement.java
@@ -0,0 +1,8 @@
+package org.firstinspires.ftc.teamcode.enums;
+
+public enum GameElement {
+    DUCK,
+    MARKER,
+    BALL,
+    CUBE
+}
